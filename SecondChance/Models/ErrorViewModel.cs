31116b04cfc3c369df778804b12637767d0b8c31
Filipe <127201137+FilipeXu@users.noreply.github.com>
2025-01-13 19:17:52 +0000

    Add project files.

---
+++ SecondChance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecondChance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
