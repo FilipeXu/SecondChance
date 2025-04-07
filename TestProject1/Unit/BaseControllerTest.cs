b4fa6b5e06d44ff53ee2019235be656b31db1ac6
Filipe <127201137+FilipeXu@users.noreply.github.com>
2025-04-07 11:20:59 +0100

    Controlador de Estatísticas

---
+++ TestProject1/Unit/BaseControllerTest.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using SecondChance.Data;
+using SecondChance.Models;
+
+namespace TestProject1.Unit;
+
+public abstract class BaseControllerTest
+{
+    protected ApplicationDbContext CreateTestContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new ApplicationDbContext(options);
+    }
+
+    protected UserManager<User> CreateTestUserManager()
+    {
+        var store = new Mock<IUserStore<User>>();
+        var optionsAccessor = new Mock<IOptions<IdentityOptions>>();
+        var passwordHasher = new Mock<IPasswordHasher<User>>();
+        var userValidators = new List<IUserValidator<User>>();
+        var passwordValidators = new List<IPasswordValidator<User>>();
+        var keyNormalizer = new Mock<ILookupNormalizer>();
+        var errors = new Mock<IdentityErrorDescriber>();
+        var services = new Mock<IServiceProvider>();
+        var logger = new Mock<ILogger<UserManager<User>>>();
+
+        optionsAccessor.Setup(o => o.Value).Returns(new IdentityOptions());
+
+        var userManager = new UserManager<User>(
+            store.Object,
+            optionsAccessor.Object,
+            passwordHasher.Object,
+            userValidators,
+            passwordValidators,
+            keyNormalizer.Object,
+            errors.Object,
+            services.Object,
+            logger.Object);
+
+        return userManager;
+    }
+
+    protected ILogger<T> CreateTestLogger<T>()
+    {
+        return new Mock<ILogger<T>>().Object;
+    }
+}
