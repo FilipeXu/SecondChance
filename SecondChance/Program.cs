141918aab6f01497cd90fc907a196ab887fd7192
Bolinha <101197147+BolinhaXD@users.noreply.github.com>
2025-04-14 19:01:52 +0100

    cicd

--- SecondChance/Program.cs
+++ SecondChance/Program.cs
@@ -8,7 +8,7 @@ using SecondChance.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("AzureConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
